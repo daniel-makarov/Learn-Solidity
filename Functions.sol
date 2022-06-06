66f64605d145a4313dd52c5105dc15730c251076
Daniel Makarov <danielmakarov.dev@gmail.com>
2022-06-06 19:42:04 -0400

    Functions

---
+++ Functions.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+contract FunctionVideo {
+
+    function multiplication(uint8 x, uint8 y) public pure returns (uint8) {
+        return x * y;
+    }
+
+}
